3604dfe3874554d11c8f7d9749f7498207de6732
cadee-exe <cadeez@nycstudents.net>
2025-11-17 09:25:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,18 +13,26 @@
 # # Calculator.add(767676, 888)
 
 class pet:
-    def __init__(self, name, money, inventory):
+    def __init__(self, name, money, inventory, food):
         self.name = name
         self.money = money
         self.inventory = inventory
-
+        self.food = food
+       
+       
     def buy(self, item):
         self.inventory.append(item)
         print(self.inventory)
 
+    def food(self, item):
+        
 
+    
 rice = pet("rice", 150, ["Potion"])
+rice.buy("cannibalism")
 
-rice.buy({'title': "cha siu", "atk": 100})
 print(rice.__dict__)
+
+
+
 
