fc4befb012df2fb9223443b2f5a5169bcc7ee6bf
cadee-exe <cadeez@nycstudents.net>
2025-11-19 09:25:10 -0500

    Update 1app.py

--- 1app.py
+++ 1app.py
@@ -11,15 +11,28 @@
 # If ANY of these rules are broken, the function should return an error message explaining what went wrong.
 # If EVERYTHING is good, the function should return a dictionary that represents the newly created user.
 
-def school_login(email, password):
-    email = input("insert email")
-    password = input("insert password")
-    
+def valid(email, password):
+
     if "@" not in email:
         return"incorrect email format"
-    if len(password) <= 8 and int(password) not in password:
-        return"invalid password format"
-    if 
+    if isinstance(email, str) or not isinstance(password, str):
+        return"Invalid: Email and password must be strings"
+    
+    return{'email': email, 'password': password}
+print(valid("test@gmail.com", "passwordmustbestring"))
+
+
+#     if len(password) <= 8 and password.isdigit not in password:
+#         for i in password:
+#             if i.isupper in password:
+#                  return"invalid password"
+#             print("invalid password")
+#         else:
+#             print("your", password, "is valid")
+#     if len(password) >= 8 and password.isdigit:
+#         return"valid password format!"
+#     print("is valid")
+# valid("test@gmail.com", "q135wsfshg")
    
 
             
