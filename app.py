94765e1ab5df7ad562f3db6c4d4d4e0ae7fc8ec7
cadee-exe <cadeez@nycstudents.net>
2025-11-25 09:26:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,60 +1,58 @@
-# class Calculator():
-#     def add(x, y):
-#         print(x + y)
-#         return x + y
+# """ # class Calculator():
+# #     def add(x, y):
+# #         print(x + y)
+# #         return x + y
 
-# #     def add_many(numbers):
-# #         print(sum(numbers))
-# #         return sum(numbers)
+# # #     def add_many(numbers):
+# # #         print(sum(numbers))
+# # #         return sum(numbers)
 
-# #     def subtract(numbers):
-# #         return numbers
+# # #     def subtract(numbers):
+# # #         return numbers
 
-# # Calculator.add(767676, 888)
+# # # Calculator.add(767676, 888) """
+
+class You:
+    def __init__(self):
+        self.name = input("you take care of rice ok so what ur name")
 
 class pet:
-    def __init__(self, name, money, inventory, food, happiness):
+    def __init__(self, name, money, inventory, food, happiness, hunger):
         self.name = name
         self.money = money
         self.inventory = inventory
         self.happiness = happiness
         self.food = food
+        self.hunger = hunger
+      
        
+        self.happiness = 0
+        self.hunger = 0
+    
+    def hungry(self, food):
+         self.hunger.append(food)
+         self.hunger +=5
+    
     def buy(self, item):
         self.inventory.append(item)
         print(self.inventory)
 
-    def happiness(self, item):
+    def happy(self, item):
         self.happiness.append(item)
         self.happiness += 10
-  
         print(f"{self.name}'s happiness is at {self.happiness}")
-     
-    def food(self, item):
-        self.hunger += 25
-        if self. food in item:
-            print(f"{self.name}'s hunger is at {self.hunger}")
-        if self.food not in item:
-            print("rice is starving. Hp -1")
-    
-        self.food == item("no food")
-        print(rice.__dict__)
 
-        
-rice = pet("atk", 150, ["Potion"],"food", "happiness")
+        self.food = ["bean", "sushi", "cheeseburger", "baguette", "pear", "beans on toast", "job application", "tomato", "grape", "pizza", "chicken", "teriyaki", "salmon", "french fries"
+                     "rice cake", "cake", "ice cream", "juice", "pibbl"]
+        self.happiness = ["fetch", "cooking", "drawing", "reading", "rolling around", "being big", "job", "working", 
+                          "school", "playing", "Roblox", "unemploynent", "money"]
+
+rice = pet("rice", 150, ["rice"],"food", "happiness", "hunger")
 
 rice.buy("cannibalism")
 rice.buy("cha siu")
 rice.buy("bao")
 print (rice.__dict__)
 
-class pet:
-    def __init__(self, name, atk):
-        self.name = name
-        self.atk = atk
     
-def display_info(self):
-    return f"User: {self.name}, atk: {self.atk}"
-
-
 
