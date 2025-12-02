aa49762b867f23c57959c83eb93715562aa57ee0
cadee-exe <cadeez@nycstudents.net>
2025-12-02 09:26:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,19 +17,19 @@ class You:
         self.name = input("you take care of rice ok so what ur name")
 
 class pet:
-    def __init__(self, name, inventory, feed, happiness, hunger, food):
+    def __init__(self, name, inventory, feed, happiness, hunger, food, live):
         self.name = name
         self.inventory = inventory
         self.happiness = happiness
         self.food = feed
         self.hunger = hunger
         self.feed = food
+        self.live = live
         
       
         self.happiness = 0
         self.hunger = 0
 
-    
     def hungry(self, food):
          self.hunger.append(food)
          self.hunger += 5
@@ -40,13 +40,13 @@ class pet:
             print("Rice is too hungry hes gonna die")
          if self.hunger > 100:
              print("ur gonna kill rice bro hes fat")
-
+        
     def buy(self, item):
         self.inventory.append(item)
         print(self.inventory)
     
 
-    def happy(self, item):
+    def happy(self, item, happiness):
         self.happiness.append(item)
         self.happiness += 10
         print(f"{self.name}'s happiness is at {self.happiness}")
@@ -56,27 +56,29 @@ class pet:
             print("rice is depressed")
         if self.happiness  > 100:
             print("rice is too jolly")
+        happiness == ["fetch", "cooking", "drawing", "reading", "rolling around", "being big", "job", "working", 
+                          "school", "playing", "Roblox", "unemploynent", "money"]
+
 
  
     eat = input("What would you like to feed rice?")
     def eats(food, hunger):
-        hunger += 5
+       
+        self.hunger += 5
         print(f"You fed Rice a {food}, Hunger increased by, {hunger}")
-        if food not in item:
+        if food not in {food}:
             print("Uh oh: food no exist. sorry!")
+    
         
-   
-            item = ["bean", "sushi", "cheeseburger", "baguette", "pear", "beans on toast", "job application", "tomato", "grape", "pizza", "chicken", "teriyaki", "salmon", "french fries"
-                     "rice cake", "cake", "ice cream", "juice", "pibbl", "ruler",
-                        "fetch", "cooking", "drawing", "reading", "rolling around", "being big", "job", "working", 
-                          "school", "playing", "Roblox", "unemploynent", "money"]
-        
-rice = pet("rice", ["rice"], "food", "happiness", "hunger", "feed")
+        food == ["bean", "sushi", "cheeseburger", "baguette", "pear", "beans on toast", "job application", "tomato", "grape", "pizza", "chicken", "teriyaki", "salmon", "french fries",
+                     "rice cake", "cake", "ice cream", "juice", "pibbl", "ruler"]
+      
+
     
 
-rice.buy("cannibalism")
-rice.buy("cha siu")
-rice.buy("bao")
+rice = pet("rice", ["rice"], "food", "happiness", "hunger", "feed", {"live= True"})
+    
+
 print (rice.__dict__)
 
     
