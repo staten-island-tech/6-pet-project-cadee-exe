4af4b46dc5a30ba931d9726b1530571cb40f7664
cadee-exe <cadeez@nycstudents.net>
2025-12-03 09:26:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,65 +21,40 @@ class pet:
         self.name = name
         self.inventory = inventory
         self.happiness = happiness
-        self.food = feed
+        self.food = food
         self.hunger = hunger
-        self.feed = food
-        self.live = live
+        self.feed = feed
+        self.live = True
         
       
-        self.happiness = 0
-        self.hunger = 0
+        self.happiness = 50
+        self.hunger = 50
 
-    def hungry(self, food):
-         self.hunger.append(food)
-         self.hunger += 5
-         print(f"{self.name}'s hunger is at {self.hunger}")
-         if self.hunger > 75:
-            print("Rice is full. any more food and hes too fat")   
-         if self.hunger < 75:
-            print("Rice is too hungry hes gonna die")
-         if self.hunger > 100:
-             print("ur gonna kill rice bro hes fat")
-        
-    def buy(self, item):
-        self.inventory.append(item)
-        print(self.inventory)
-    
-
-    def happy(self, item, happiness):
-        self.happiness.append(item)
-        self.happiness += 10
-        print(f"{self.name}'s happiness is at {self.happiness}")
-        if self.happiness > 50:
-            print("rice is happy")
-        if self.happiness < 50:
-            print("rice is depressed")
-        if self.happiness  > 100:
-            print("rice is too jolly")
-        happiness == ["fetch", "cooking", "drawing", "reading", "rolling around", "being big", "job", "working", 
-                          "school", "playing", "Roblox", "unemploynent", "money"]
+    def hungry(self):
+         while self.live == True:
+            if self.hunger >= 50 and self.hunger <= 100:
+                print(f"{self.name} is not very hungry, hunger is at {self.hunger}")
+                break
+            elif self.hunger < 50:
+                print(f"{self.name} is starving, hunger at {self.hunger}")
+                break
+            elif 
 
 
- 
-    eat = input("What would you like to feed rice?")
-    def eats(food, hunger):
-       
-        self.hunger += 5
-        print(f"You fed Rice a {food}, Hunger increased by, {hunger}")
-        if food not in {food}:
-            print("Uh oh: food no exist. sorry!")
-    
-        
-        food == ["bean", "sushi", "cheeseburger", "baguette", "pear", "beans on toast", "job application", "tomato", "grape", "pizza", "chicken", "teriyaki", "salmon", "french fries",
-                     "rice cake", "cake", "ice cream", "juice", "pibbl", "ruler"]
+    def happy(self):
+        while self.live == True:
+            
+            play = input("What would you like to do with fish?")
+      
+
       
 
     
 
-rice = pet("rice", ["rice"], "food", "happiness", "hunger", "feed", {"live= True"})
-    
+fish = pet("fish", ["fish"], "food", "happiness", "hunger", "feed", {"live= True"})
 
-print (rice.__dict__)
-
+fish.__dict__()
+fish.hunger()
+fish.happiness()
     
 
